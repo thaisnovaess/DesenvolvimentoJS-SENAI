f57b572e4bf64fa51d9d1de5ddda660b41fcd12d
Thais Novaes <thaisnovaes.ar@gmail.com>
2025-08-12 17:05:51 -0300

    .

---
+++ Aula 11 - CondicoesEncadeadas/exemploJS/media.js
@@ -0,0 +1,11 @@
+let nota1 = 80 
+let nota2 = 80
+let media = (nota1 = nota2) / 2
+
+if(media >= 70) {
+    console.log("APROVADO")    
+} else if(media >= 50) {
+    console.log("Recuperação");
+} else {
+    console.log("Reprovado");
+}
