3de19c72f3421552c3e9ab2da8341198ad1d655a
Thais Novaes <thaisnovaes.ar@gmail.com>
2025-08-21 13:37:03 -0300

    .

---
+++ Aula 14/script.js
@@ -0,0 +1,29 @@
+//Variável composta
+let produto = {
+    tipo: "Notebook",
+    preco: 3500,
+    marca: "Positivo",
+    processador: "Intel Celeron",
+    emEstoque: true
+}
+
+
+console.log(produto)
+
+
+//Exibir a informação de um item no objeto
+console.log(produto.marca) //Positivo
+console.log(produto.preco) //3500
+console.log(produto["tipo"]) //Notebook
+console.log(produto["processador"]) //Intel Celeron
+
+//Adicionar itens no objeto 
+produto.memoriaRam = "4 gb"
+console.log(produto);
+
+//Remover item
+delete produto 
+
+
+//Diferente do array o objeto faz descrição para identificar cada valor salvo, com a {}.
+
